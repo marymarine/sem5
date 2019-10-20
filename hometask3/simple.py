383449a6e594f25de83ab8f8d0d6358564e892c7
marymarine <55350748+marymarine@users.noreply.github.com>
2019-10-21 02:51:33 +0300

    Add files via upload

---
+++ hometask3/simple.py
@@ -0,0 +1,39 @@
+"""Sequential program execution"""
+
+import os
+import time
+import warnings
+import librosa
+import numpy as np
+
+warnings.filterwarnings('ignore')
+
+def extract_mfccs(test_dir, res_dir):
+    """Search audio files (*.m4a) and create numpy arrays"""
+    for name in os.listdir(test_dir):
+        if name[-4:] == '.m4a':
+            path = os.path.join(test_dir, name)
+            #generate mfccs
+            y, sr = librosa.load(path)
+            mfcc = librosa.feature.mfcc(y, sr)
+            #save an array to a binary file in .npy format
+            path = os.path.join(res_dir, name[0:-4])
+            np.save(path, mfcc)
+        else:
+            #create directories in the resulting directory
+            res_path = os.path.join(res_dir, name)
+            os.mkdir(res_path)
+            #execute the next iteration
+            test_path = os.path.join(test_dir, name)
+            extract_mfccs(test_path, res_path)
+            continue
+
+if __name__ == '__main__':
+    start_time = time.time()
+    #create the resulting directory
+    res = os.path.join(os.getcwd(), 'result')
+    os.mkdir(res)
+    #create numpy arrays
+    extract_mfccs(os.path.abspath("test"), res)
+    #get program execution time
+    print(time.time() - start_time)
